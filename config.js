3b7ca1215f67500d05b717cc553dfb7e6e76b46e
Zukunftspreis <zukunftspreis@makerport.de>
2026-09-03 10:09:29 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// Project Settings -> API. Diese Datei liegt im Website-Root neben index.html.
+
+window.SUPABASE_URL = 'https://ipackdcbbrkicyinytdg.supabase.co';
+window.SUPABASE_ANON_KEY = 'sb_publishable_36BhVKqGkRJIBxthX4D94g_EPKb42tD';
